c2dc61dfd65ff1dcb3309cbe83b627032fbe115e
David S. Garcia (Azæl) <azael@dragonspark.io>
2025-02-17 21:20:50 -0500

    chore: Initialize tooling and npm package
    Added Prettier configuration with specific rules for code formatting. Initialized package.json with necessary scripts and devDependencies. Also, created a lock file to ensure consistent installation of dependencies across environments.

---
+++ rollup.config.js
@@ -0,0 +1,55 @@
+﻿import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
+
+/** @type {import('rollup').RollupOptions} */
+const config = {
+  input: 'src/index.ts',
+  output: [
+    // ESM bundle
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    // CommonJS bundle
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    // Minified ESM bundle (.min)np
+    {
+      file: 'dist/index.esm.min.js',
+      format: 'esm',
+      plugins: [terser({
+        ecma: 5,
+        module: true,
+        toplevel: true,
+        compress: { pure_getters: true },
+        format: { wrap_func_args: false },
+      })],
+      sourcemap: true
+    },
+    // Minified CommonJS bundle (.min)
+    {
+      file: 'dist/index.cjs.min.js',
+      format: 'cjs',
+      plugins: [terser({
+        ecma: 5,
+        module: true,
+        toplevel: true,
+        compress: { pure_getters: true },
+        format: { wrap_func_args: false },
+      })],
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json',
+      clean: true
+    })
+  ]
+};
+
+export default config;
