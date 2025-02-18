2990e837a5d14b59c428f52ab773115018b195cf
David S. Garcia (Azæl) <azael@dragonspark.io>
2025-02-17 21:22:45 -0500

    feat(adaptation): Add chromatic adaptation functionality
    Introduced new files for handling chromatic adaptation. This includes the computation of the chromatic adaptation matrix to transform colors from a source illuminant to a target illuminant using a specified cone response model. Also added precomputed Bradford matrices for D50 to D65 and vice versa, along with their respective cone response models. The changes also include methods for dynamically computing the matrix if no precomputed version exists.

---
+++ src/adaptation/chromatic-adaptation.ts
@@ -0,0 +1,56 @@
+﻿import type { Illuminant } from '../standards/illuminants';
+import type { ConeResponseModel } from './cone-response';
+import { type Matrix3x3, multiplyMatrices, multiplyMatrixByVector } from '../utils/linear';
+import { coneMatrixes } from './_cone-matrixes';
+
+/**
+ * Computes the chromatic adaptation matrix to transform colors from a source illuminant
+ * to a target illuminant using a specified cone response model.
+ *
+ * @param {Illuminant} sourceIlluminant - The source illuminant, represented by its XYZ tri-stimulus values.
+ * @param {Illuminant} targetIlluminant - The target illuminant, represented by its XYZ tri-stimulus values.
+ * @param {ConeResponseModel} coneModel - The cone response model containing transformation matrices for converting between
+ *                                         XYZ and LMS color spaces as well as their inverses.
+ * @returns {Matrix3x3} The 3x3 chromatic adaptation matrix that can be used to adapt colors from the source illuminant
+ *                      to the target illuminant.
+ *
+ * @see https://en.wikipedia.org/wiki/Chromatic_adaptation
+ */
+export const computeAdaptationMatrix = (sourceIlluminant: Illuminant, targetIlluminant: Illuminant, coneModel: ConeResponseModel): Matrix3x3 => {
+  const xyzTarget = [sourceIlluminant.x, sourceIlluminant.y, sourceIlluminant.z];
+  const xyzSource = [targetIlluminant.x, targetIlluminant.y, targetIlluminant.z];
+
+  const lmsSource = multiplyMatrixByVector(coneModel.matrix, xyzSource);
+  const lmsTarget = multiplyMatrixByVector(coneModel.matrix, xyzTarget);
+
+  const scaleL = lmsSource[0] / lmsTarget[0];
+  const scaleM = lmsSource[1] / lmsTarget[1];
+  const scaleS = lmsSource[2] / lmsTarget[2];
+
+  const scalingFactorMatrix: Matrix3x3 = [
+    [scaleL, 0, 0],
+    [0, scaleM, 0],
+    [0, 0, scaleS]
+  ];
+
+  return multiplyMatrices(coneModel.inverseMatrix, scalingFactorMatrix, coneModel.matrix) as Matrix3x3;
+};
+
+/**
+ * Calculates the chromatic adaptation matrix for converting colors from a source illuminant to a target illuminant
+ * using a specific cone response model. If a precomputed matrix exists for the specified parameters, it is returned.
+ * Otherwise, the matrix is computed dynamically.
+ *
+ * @param {Illuminant} sourceIlluminant - The illuminant representing the source light condition.
+ * @param {Illuminant} targetIlluminant - The illuminant representing the target light condition.
+ * @param {ConeResponseModel} coneModel - The cone response model to be used for the adaptation.
+ * @returns {Matrix3x3} The 3x3 chromatic adaptation matrix.
+ */
+export const getAdaptationMatrix = (sourceIlluminant: Illuminant, targetIlluminant: Illuminant, coneModel: ConeResponseModel): Matrix3x3 => {
+  const precomputedMatrix: Matrix3x3 | undefined = coneMatrixes[`${coneModel.name}_${sourceIlluminant.name}_TO_${targetIlluminant.name}`];
+  if (precomputedMatrix) {
+    return precomputedMatrix;
+  }
+
+  return computeAdaptationMatrix(sourceIlluminant, targetIlluminant, coneModel);
+};
---
+++ src/adaptation/cone-response.ts
@@ -0,0 +1,35 @@
+﻿import type { Matrix3x3 } from '../utils/linear';
+
+export type ConeResponseModel = {
+  name: string;
+  matrix: Matrix3x3;
+  inverseMatrix: Matrix3x3;
+}
+
+export const XYZScalingConeModel: ConeResponseModel = {
+  name: 'XYZ_SCALING',
+  matrix: [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ],
+  inverseMatrix: [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ]
+};
+
+export const BradfordConeModel: ConeResponseModel = {
+  name: 'BRADFORD',
+  matrix: [
+    [0.8951, 0.2664, -0.1614],
+    [-0.7502, 1.7135, 0.0367],
+    [0.0389, -0.0685, 1.0296]
+  ],
+  inverseMatrix: [
+    [0.9869929, -0.1470543, 0.1599627],
+    [0.4323053, 0.5183603, 0.0492912],
+    [-0.0085287, 0.0400428, 0.9684867]
+  ]
+};
