3ed025d86b34c6842267fd035e1f065d50008337
David S. García (Azæl) <azael@dragonspark.io>
2025-05-23 18:19:58 -0500

    build: Update build script and config management
    Use tsup config file for streamlined build process.
    Exclude 'node_modules' in tsconfig.json for clarity.
    Introduce tsup.config.ts for custom build settings.

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/fn.ts', '!src/__tests__/**', '!src/**/*.test.*'],
+  format: ['esm'],
+  external: ['vitest', '@vitest/ui', '@types/node'],
+  target: 'es6',
+  dts: true,
+  experimentalDts: false
+});
